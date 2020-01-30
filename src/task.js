10707f86a7c18195b3ade51821617a94fcd021a9
IvanPustovit <pustovit.for@gmail.com>
2020-01-30 14:06:49 +0200

    Homework8

---
+++ src/task.js
@@ -0,0 +1,60 @@
+'use scrict'
+// Разбей задание на несколько подзадач:
+
+// Создание и рендер разметки по массиву данных и предоставленному шаблону.
+// Реализация делегирования на галерее ul.js-gallery и получение url большого изображения.
+// Открытие модального окна по клику на элементе галереи.
+// Подмена значения атрибута src элемента img.lightbox__image.
+// Закрытие модального окна по клику на кнопку button[data-action="close-modal"].
+// Очистка значения атрибута src элемента img.lightbox__image. Это необходимо для того, чтобы при следующем открытии модального окна, пока грузится изображение, мы не видели предыдущее.
+
+import gallery from './gallery-items.js'
+
+const galleryJs = document.querySelector('.js-gallery')
+const lightbox = document.querySelector('.js-lightbox')
+const closeBtn = document.querySelector('[data-action="close-lightbox"]')
+const imgLightbox = document.querySelector('.lightbox__image')
+
+
+//добавление картинок в разметку
+function imgAdd(arr) {
+    for (let obj of arr) {
+        let objGallery = obj;
+        const link = document.createElement('a')
+        const list = document.createElement('li')
+        const img = document.createElement('img')
+        list.append(link)
+        link.append(img)
+        list.classList.add('gallery__item')
+        link.classList.add('gallery__link')
+        img.classList.add('gallery__image')
+        link.setAttribute('href', objGallery['original'])
+        img.setAttribute('src', objGallery['preview'])
+        img.setAttribute('data-source', objGallery['original'])
+        img.setAttribute('alt', objGallery['description'])
+        galleryJs.insertAdjacentHTML('afterbegin', `${list.outerHTML}`)
+    }
+    return galleryJs
+}
+imgAdd(gallery);
+
+
+//модальное окно
+function bigImg(e) {
+    e.preventDefault()
+    if (e.target.nodeName === 'IMG') {
+        lightbox.classList.add('is-open');
+        imgLightbox.src = e.target.dataset.source;
+        imgLightbox.alt = e.target.alt;
+    }
+}
+
+//кнопка закрытия модального окна
+function btnClosed() {
+    lightbox.classList.remove('is-open')
+    imgLightbox.src = '';
+    imgLightbox.alt = '';
+}
+
+galleryJs.addEventListener('click', bigImg)
+closeBtn.addEventListener('click', btnClosed)
